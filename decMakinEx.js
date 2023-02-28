3147a4818cebab6a3147ef0ad97528bf6dfc5edf
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-02-28 12:10:38 -0600

    I am trying I am sorry.

---
+++ decMakinEx.js
@@ -0,0 +1,30 @@
+//  1a
+let randomNum = Math.random();
+console.log(randomNum);
+
+randomNumber = 5
+const randomNumber = Math.floor();
+console.log(randomNumber);
+
+// 1b
+if (randomNum === 1) {
+    print('Its equal to 1');
+} else if (randomNum === 2,3){
+    print('Its equal to 2 or 3');
+} else if (randomMun <= 4){
+    print('Greater or equal to 4');
+} else {
+    print('Its not mathing');
+}
+// 1c
+if (randomNum = 3 = false){
+    print('Not equal to three');
+}
+// 1d
+if (randomNum = 3, 5 = false){
+    print('Not equal to three or five.');
+}
+// 1e
+if (randomNum = 2, 4){
+    print('Equal to 2 and/or 4');
+}
